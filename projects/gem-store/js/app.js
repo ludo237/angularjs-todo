0a3ddbb028bad3cfb08a21b57046afb99a98a87c
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-05-17 09:30:15 +0200

    [ Update ] app.js
    Product controller now is inside productSection directive

--- projects/gem-store/js/app.js
+++ projects/gem-store/js/app.js
@@ -21,7 +21,22 @@
     app.directive('productSection', function() {
         return {
             restrict : 'E',
-            templateUrl : 'views/product/section.html'
+            templateUrl : 'views/product/section.html',
+            // We need to specify the controller for our section, to keep the code clean
+            controller : function() {
+                // This is the equivalent of ng-init = 1
+                this.tab = 1;
+                // This is the equivalento of ng-click="tab = n"
+                this.selectTab = function (setTab) {
+                    this.tab = setTab;
+                };
+                // This is the equivalent of { active: tab == 1} for setting stuff
+                this.isSelected = function (checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            // Also the alias
+            controllerAs: 'panel'
         };
     });
 
@@ -32,6 +47,7 @@
         this.products = gems;
     });
 
+    /* We have moved this inside the directive
     app.controller('PanelController', function() {
         // This is the equivalent of ng-init = 1
         this.tab = 1;
@@ -43,7 +59,8 @@
         this.isSelected = function (checkTab) {
             return this.tab === checkTab;
         };
-    });
+    });*/
+
     // Controller for our reviews form
     app.controller('ReviewController', function() {
         // Initialize the review variable used in view
