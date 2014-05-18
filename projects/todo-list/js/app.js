45e65e905d6c2917f9ed85b2457643a05f0993cd
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-05-18 10:54:07 +0200

    [ Update ] Add a new Controller
    UserController will be very useful

--- projects/todo-list/js/app.js
+++ projects/todo-list/js/app.js
@@ -1,4 +1,5 @@
 (function(){
+    'use strict';
     // Init the module
     var app = angular.module('todo', []);
 
@@ -12,6 +13,14 @@
         };
     });
 
+    app.controller('UserController', function() {
+        var user = this;
+        user.data = {
+            name : null,
+            enable : true
+        };
+    });
+
     /**
     * TodoController
     * @dependencies  $http
@@ -22,9 +31,10 @@
         // Initialize the empty array for tasks
         todo.tasks = [];
         // fetch tasks from our storage
+        /*  We are using local storage so .. no json file at the moment...
         $http.get('data/tasks.json').success(function(data) {
             todo.tasks = data;
-        });
+        });*/
     }]);
 
 })();
