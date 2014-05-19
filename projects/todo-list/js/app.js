18cc79faad1b4186daa4ac917d5a20dfb728d9f7
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-05-19 16:41:51 +0200

    [ Update ] index.html new functions
    You can delete all todos, create new one … mostly LoL

--- projects/todo-list/js/app.js
+++ projects/todo-list/js/app.js
@@ -3,6 +3,13 @@
     // Init the module
     var app = angular.module('todo', []);
 
+    // Global tasks
+    var tasks = [];
+
+    /**
+    * TestController
+    * @description Check if localStorage is available
+    **/
     app.controller('TestController', function() {
         this.supportStorage = function() {
             try {
@@ -13,33 +20,70 @@
         };
     });
 
+    /**
+    * UserController
+    * @description Handle the basic function for the user
+    **/
     app.controller('UserController', function($scope) {
-        $scope.data = {
-            name : 'guest_1234',
-            enable : false
-        };
+        // Actions that can be done by the user
+        $scope.action = 0; // None taken 1 for add todo 2 for delete all
+        // Initialize the user data
+        if( localStorage['todo.user.enable'] == "true") {
+            $scope.data = {
+                name : localStorage['todo.user.name'],
+                enable : localStorage['todo.user.enable']
+            }
+        }  else {
+             $scope.data = {
+                name : 'guest_1234',
+                enable : false
+            };
+        }
         $scope.updateUser = function() {
             // Update the user's name
             $scope.data.name = this.data.name;
             // Enable the user
             $scope.data.enable = true;
+            // Update localStorage
+            localStorage['todo.user.name'] = this.data.name;
+            localStorage['todo.user.enable'] = true;
+        };
+        // Set the current action
+        $scope.setAction = function(action) {
+            $scope.action = action;
+        };
+        // Check the current action
+        $scope.getAction = function(action) {
+            return  $scope.action === action;
+        };
+        // Delete all todos
+        $scope.cleanUp = function() {
+            localStorage['todo.tasks'] = [];
+            tasks = [];
+            alert("Todos deleted!");
+        };
+        // Save current Task
+        $scope.newTask = function() {
+            var newTask = { 'description': $scope.task.description, 'done': false, 'created_at': Date.now(), 'updated_at': Date.now() };
         };
     });
 
     /**
     * TodoController
-    * @dependencies  $http
+    * @Handler the todo tab
     **/
-    app.controller('TodoController', [ '$http', function($http) {
-        // Cache this attribute to use is with $http
-        var todo = this;
+    app.controller('TodoController', function($scope) {
         // Initialize the empty array for tasks
-        todo.tasks = [];
-        // fetch tasks from our storage
-        /*  We are using local storage so .. no json file at the moment...
-        $http.get('data/tasks.json').success(function(data) {
-            todo.tasks = data;
-        });*/
-    }]);
+        if(localStorage['todo.tasks']) {
+            console.log("Resumed your tasks");
+            tasks.push(localStorage['todo.tasks']);
+        } else {
+            console.log("Init new tasks object");
+            localStorage['todo.tasks'] = JSON.stringify( [] );
+            tasks = localStorage['todo.tasks'];
+        }
+        // Grab all the tasks
+        $scope.tasks = JSON.parse(tasks);
+    });
 
 })();
